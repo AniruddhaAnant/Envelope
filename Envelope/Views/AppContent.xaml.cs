f629e44cb34ea8a7498df52bf43df01057c73eac
sparkskapil <sparkskapil@gmail.com>
2019-07-10 23:20:55 +0530

    Ui changes (#1)
    * UIChanges
    
    * Implemented Import Files and Other Bug Fixes

--- Envelope/Views/AppContent.xaml.cs
+++ Envelope/Views/AppContent.xaml.cs
@@ -24,5 +24,33 @@ namespace Envelope.Views
         {
             InitializeComponent();
         }
+
+        //private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ButtonCloseMenu.Visibility = Visibility.Visible;
+        //    ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        //}
+
+        //private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        //    ButtonOpenMenu.Visibility = Visibility.Visible;
+        //}
+        //private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    UserControl usc = null;
+        //    GridMain.Children.Clear();
+
+        //    switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+        //    {
+        //        case "ItemHome":
+        //            usc = new AddFolderView();
+        //            GridMain.Children.Add(usc);
+        //            break;
+
+        //        default:
+        //            break;
+        //    }
+        //}
     }
 }
